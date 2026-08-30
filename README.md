08ca9d19f51732ef429bdb44df0dcf3729fd0a07
Raghava <macbook@Raghava.local>
2026-08-30 12:39:43 +0530

    python assignemnt flask app

---
+++ README.md
@@ -0,0 +1,32 @@
+# Flask & MongoDB Assignment
+
+A simple Flask application that serves a JSON API and includes a frontend form that securely inserts data into MongoDB.
+
+## Prerequisites
+- Python 3.x
+- Mongodb URI
+
+## Setup Instructions
+
+1. **Clone the repository:**
+
+2. **Set up the virtual environment:**
+   python3 -m venv venv
+   source venv/bin/activate
+
+3. **Install the requirements:**
+   pip install -r requirements.txt
+
+
+4. **Configure Environment Variables:**
+   MONGO_URI
+
+## Running the Application
+Start the Flask server by running:
+python app.py
+The server will start on `http://127.0.0.1:8000/`.
+
+## Routes
+- `GET /` - Renders the form to collect user name and email.
+- `POST /` - Accepts form submission and saves the data to MongoDB. Redirects to `/success` on success.
+- `GET /api` - Returns JSON data loaded from `movies.json`.
---
+++ app.py
@@ -0,0 +1,58 @@
+from flask import Flask, jsonify, request, render_template, redirect, url_for
+from pymongo import MongoClient
+import json
+import os
+from dotenv import load_dotenv
+load_dotenv()
+
+app = Flask(__name__)
+MONGO_URI = os.getenv("MONGO_URI")
+
+if not MONGO_URI:
+    print("MONGO_URI is not set in ENV.")
+
+try:
+    client = MongoClient(MONGO_URI)
+    db = client['tutedude_db']
+    collection = db['users']
+except Exception as e:
+    print(f"Failed to connect to MongoDB: {e}")
+
+@app.route('/', methods=['GET', 'POST'])
+def index():
+    error = None
+    if request.method == 'POST':
+        name = request.form.get('name')
+        email = request.form.get('email')
+        
+        if not name or not email:
+            error = "Name and Email are required!"
+        else:
+            try:
+                collection.insert_one({"name": name, "email": email})
+                return redirect(url_for('success'))
+            except Exception as e:
+                error = f"Database error: {str(e)}"
+    return render_template('index.html', error=error)
+
+@app.route('/success')
+def success():
+    return render_template('success.html')
+
+@app.route('/api', methods=['GET'])
+def get_api_data():
+    try:
+        base_dir = os.path.dirname(os.path.abspath(__file__))
+        json_path = os.path.join(base_dir, 'movies.json')
+        
+        with open(json_path, 'r') as file:
+            data = json.load(file)
+            
+        return jsonify(data), 200
+    except FileNotFoundError:
+        return jsonify({"error": "Data file not found"}), 404
+    except Exception as e:
+        return jsonify({"error": str(e)}), 500
+
+if __name__ == '__main__':
+    app.run(debug=True, port=8000)
